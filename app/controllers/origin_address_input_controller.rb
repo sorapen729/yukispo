7959d272ac70e9c03f269239fdef24245bda7f07
sorapen729 <sorapen729@gmail.com>
2025-09-09 22:08:44 +0900

    住所入力画面の仮作成＆コントローラーの編集

--- app/controllers/origin_address_input_controller.rb
+++ app/controllers/origin_address_input_controller.rb
@@ -3,5 +3,15 @@ class OriginAddressInputController < ApplicationController
   end
 
   def create
+    if params[:origin_address][:address].present?
+      session[:origin_address] = {
+        address: params[:origin_address][:address],
+        lat: params[:origin_address][:lat],
+        lng: params[:origin_address][:lng]
+      }
+      redirect_to root_path, notice: '出発地が設定されました'
+    else
+      redirect_to origin_address_input_path, alert: '住所を入力してください'
+    end
   end
 end
