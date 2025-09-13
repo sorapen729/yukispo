85a5fde3a475ad35d46f170b357e0085d57b0f16
sorapen729 <sorapen729@gmail.com>
2025-09-13 22:14:40 +0900

    条件絞り込み画面の仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   resource :origin_selection, only: %i[new]
   resources :origin_address_inputs, only: %i[new create]
-
+  resource :search_filter, only: %i[new]
   # Defines the root path route ("/")
   # 既存
   # root "posts#index"
