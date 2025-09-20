ca8cd3678a929fe13a3f13bd0d37dd4a0263fbe8
sorapen729 <sorapen729@gmail.com>
2025-09-20 12:00:28 +0900

    フォーム入力時エラー情報を個別表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true, length: { maximum: 6 }
   validates :last_name, presence: true, length: { maximum: 6 }
+  validates :first_name, presence: true, length: { maximum: 6 }
 end
