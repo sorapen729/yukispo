545eb1fd540ba1014c75bcf644709ed59d524ddd
sorapen729 <sorapen729@gmail.com>
2025-09-26 07:04:05 +0900

    add/独自ドメインのホストの許可設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # 独自ドメインのホストの許可
+  config.host << "www.yukispo.com"
 end
