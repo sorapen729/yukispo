8c3d9c934704fb62ad5840262d912ecb9c82c643
sorapen729 <sorapen729@gmail.com>
2025-10-10 21:09:34 +0900

    spec/models/user_spec.rbの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,111 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
+  describe 'バリデーションチェック' do
     subject { build(:user) }
 
-    context 'last_name' do
-      it 'is valid with a valid last_name' do
+    context '姓' do
+      it '姓がある場合にvalidになるか' do
         user = build(:user, last_name: '山田')
         expect(user).to be_valid
       end
 
-      it 'is invalid without last_name' do
+      it '姓がない場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, last_name: nil)
         expect(user).to be_invalid
-        expect(user.errors[:last_name]).to include('は6文字以内で入力してください')
+        expect(user.errors[:last_name]).to include('は6文字以下で入力してください')
       end
 
-      it 'is invalid with last_name longer than 6 characters' do
+      it '姓が6文字を超える場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, last_name: 'あ' * 7)
         expect(user).to be_invalid
         expect(user.errors[:last_name]).to include('は6文字以下で入力してください')
       end
 
-      it 'is valid with last_name of 6 characters' do
+      it '姓が6文字以内の場合にvalidになるか' do
         user = build(:user, last_name: 'あ' * 6)
         expect(user).to be_valid
       end
     end
 
-    context 'first_name' do
-      it 'is valid with a valid first_name' do
+    context '名' do
+      it '名がある場合にvalidになるか' do
         user = build(:user, first_name: '太郎')
         expect(user).to be_valid
       end
 
-      it 'is invalid without first_name' do
+      it '名がない場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, first_name: nil)
         expect(user).to be_invalid
-        expect(user.errors[:first_name]).to include('は6文字以内で入力してください')
+        expect(user.errors[:first_name]).to include('は6文字以下で入力してください')
       end
 
-      it 'is invalid with first_name longer than 6 characters' do
+      it '名が6文字を超える場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, first_name: 'あ' * 7)
         expect(user).to be_invalid
         expect(user.errors[:first_name]).to include('は6文字以下で入力してください')
       end
 
-      it 'is valid with first_name of 6 characters' do
+      it '名が6文字以内の場合にvalidになるか' do
         user = build(:user, first_name: 'あ' * 6)
         expect(user).to be_valid
       end
     end
 
-    context 'email' do
-      it 'is valid with a valid email' do
+    context 'メールアドレス' do
+      it 'メールアドレスがある場合にvalidになるか' do
         user = build(:user, email: 'test@example.com')
         expect(user).to be_valid
       end
 
-      it 'is invalid without email' do
+      it 'メールアドレスがない場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, email: nil)
         expect(user).to be_invalid
       end
 
-      it 'is invalid with duplicate email' do
+      it '一意なメールアドレスでない場合はバリデーションが機能してinvalidになるか' do
         create(:user, email: 'duplicate@example.com')
         user = build(:user, email: 'duplicate@example.com')
         expect(user).to be_invalid
       end
     end
 
-    context 'password' do
-      it 'is invalid without password' do
+    context 'パスワード' do
+      it 'パスワードがない場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, password: nil)
         expect(user).to be_invalid
       end
 
-      it 'is invalid with password shorter than 6 characters' do
+      it 'パスワードが6文字未満の場合にバリデーションが機能してinvalidになるか' do
         user = build(:user, password: '12345', password_confirmation: '12345')
         expect(user).to be_invalid
       end
 
-      it 'is valid with password of 6 or more characters' do
+      it 'パスワードが6文字以上の場合にvalidになるか' do
         user = build(:user, password: '123456', password_confirmation: '123456')
         expect(user).to be_valid
       end
     end
   end
 
-  describe 'devise modules' do
-    it 'includes database_authenticatable' do
+  describe 'Deviseモジュール' do
+    it 'database_authenticatableを含むこと' do
       expect(User.devise_modules).to include(:database_authenticatable)
     end
 
-    it 'includes registerable' do
+    it 'registerableを含むこと' do
       expect(User.devise_modules).to include(:registerable)
     end
 
-    it 'includes recoverable' do
+    it 'recoverableを含むこと' do
       expect(User.devise_modules).to include(:recoverable)
     end
 
-    it 'includes rememberable' do
+    it 'rememberableを含むこと' do
       expect(User.devise_modules).to include(:rememberable)
     end
 
-    it 'includes validatable' do
+    it 'validatableを含むこと' do
       expect(User.devise_modules).to include(:validatable)
     end
   end
