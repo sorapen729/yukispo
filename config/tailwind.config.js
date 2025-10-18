112d79e9d9a44029a7be1c7dcbf01f712923a826
sorapen729 <sorapen729@gmail.com>
2025-10-18 18:16:00 +0900

    DaisyUIの導入完了

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
   },
   plugins: [
+    require('daisyui'),
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
